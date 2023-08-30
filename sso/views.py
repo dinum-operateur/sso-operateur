5da089ecb80d87822152d0f9b3545673c5108735
Agnès Haasser <agnes.haasser@beta.gouv.fr>
2023-08-30 11:14:25 +0200

    Update csp in multi-login view

--- sso/views.py
+++ sso/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from sso.models import AutologinClient
+
 
 def view_index(request):
     return render(request, "sso/accueil.html")
@@ -10,4 +12,22 @@ def view_accessibilite(request):
 
 
 def view_multilogin(request):
-    return render(request, "sso/oidc/multi-login.html")
+    """
+    View to log in all oidc autologin clients at once.
+    Partially similar to hook do_autologin_after_successful_login
+    """
+    # avoid endless loop in iframes, like the hook
+    request.session["autologin_initiated"] = True
+    # get all autologin clients, unlike the hook
+    autologin_clients = AutologinClient.objects.all()
+
+    response = render(
+        request,
+        "sso/oidc/multi-login.html",
+        {"uri": "", "other_autologin_clients": autologin_clients},
+    )
+    # allow all clients to be displayed in iframes
+    response._csp_update = {
+        "frame-src": " ".join(c.autologin_url for c in autologin_clients)
+    }
+    return response
