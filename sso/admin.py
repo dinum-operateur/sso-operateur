b2f51cab21fd3d772e47f9650495265dedc71fde
Agnès Haasser <agnes.haasser@beta.gouv.fr>
2023-09-12 11:43:37 +0200

    Display Users in SSO admin

--- sso/admin.py
+++ sso/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from sso.models import AutologinClient
+from sso.models import AutologinClient, User
 
 
 @admin.register(AutologinClient)
 class AutologinClientAdmin(admin.ModelAdmin):
     list_display = ["id", "oidc_client"]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+        "is_staff",
+    )
